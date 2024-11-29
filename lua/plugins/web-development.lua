678a559f677b5b2c8c0b14e9199fcb4b34cb4a76
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2024-11-30 00:57:27 +0100

    Install HTML and CSS LSPs, recognize liquid

---
+++ lua/plugins/web-development.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "html",
+        "cssls",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        html = {},
+        cssls = {},
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      vim.filetype.add({
+        extension = {
+          liquid = "html",
+        },
+      })
+    end,
+  },
+}
