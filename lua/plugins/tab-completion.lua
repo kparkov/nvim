569167ca2321bac8f674a8b9f8264828ef2bc8f1
Kristian Videmark Parkov <parkov@bitkompagniet.dk>
2024-12-24 02:46:22 +0100

    Make tab complete in blink

--- lua/plugins/tab-completion.lua
+++ lua/plugins/tab-completion.lua
@@ -2,20 +2,12 @@
 
 return {
   {
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      local cmp = require("cmp")
-
-      -- Use the old settings, but override using these
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        -- <Tab>: Confirm the current selection
-        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-
-        -- Disable the enter selects behaviour
-        ["<CR>"] = cmp.mapping(function(fallback)
-          fallback()
-        end, { "i", "s" }),
-      })
-    end,
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        ["<Tab>"] = { "accept" },
+        ["<CR>"] = {},
+      },
+    },
   },
 }
